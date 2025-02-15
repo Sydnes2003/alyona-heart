6fd9f6ac362a42c30598b3756a03dcbc581a52a8
Sydnes <136016959+Sydnes2003@users.noreply.github.com>
2025-02-15 06:28:11 +0300

    Animation

--- src/HeartText.tsx
+++ src/HeartText.tsx
@@ -1,10 +1,20 @@
-import { useEffect, useState } from 'react';
+import { useEffect, useRef, useState } from 'react';
 import "./HeartText.css";
 import koshka from "./assets/koshka.png";
+
+interface Heart {
+    x: number;
+    y: number;
+}
 
 const HeartText = ({ text = "Имя", size = 15, scaleY = 1, speed = 100 }) => {
     // eslint-disable-next-line react-hooks/rules-of-hooks
     const [offset, setOffset] = useState(0);
+    // eslint-disable-next-line react-hooks/rules-of-hooks
+    const [hearts, setHearts] = useState<Heart[]>([]);
+    // Attach a ref to the heart container
+    // eslint-disable-next-line react-hooks/rules-of-hooks
+    const containerRef = useRef<HTMLDivElement | null>(null);
 
     // eslint-disable-next-line react-hooks/rules-of-hooks
     useEffect(() => {
@@ -30,18 +40,40 @@ const HeartText = ({ text = "Имя", size = 15, scaleY = 1, speed = 100 }) => {
         heartShape.push(row.join(""));
     }
 
+    const handleClick = () => {
+        // Make sure we have the container dimensions
+        if (containerRef.current) {
+            const { width, height } = containerRef.current.getBoundingClientRect();
+            const randomX = Math.floor(Math.random() * width);
+            const randomY = Math.floor(Math.random() * height);
+            setHearts((prevHearts) => [...prevHearts, { x: randomX, y: randomY }]);
+        }
+    };
+
     return (
         <div className="heart-wrapper">
             <h1 className="title">С ДНЁМ СВЯТОГО ВАЛЕНТИНА!</h1>
             <div className="image-container">
-                <img src={koshka} alt="Кошка" className="circle-image" />
+                <img src={koshka} alt="Кошка" className="circle-image" title="Красавица, Няшка, Тролль и Кошка ❤️"/>
             </div>
-            <div className="heart-container">
+            <div className="heart-container" onClick={handleClick} ref={containerRef}>
                 {heartShape.map((line, index) => (
                     <div key={index} className="heart-line">
                         {line.split("").map((char, i) => (
                             <span key={i} className="heart-char">{char}</span>
                         ))}
+                    </div>
+                ))}
+                {hearts.map((heart, index) => (
+                    <div
+                        key={index}
+                        className="heart-emoji"
+                        style={{
+                            left: `${heart.x}px`,
+                            top: `${heart.y}px`,
+                        }}
+                    >
+                        ❤️
                     </div>
                 ))}
             </div>
